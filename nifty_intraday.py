512ccf19910b08c8a551a7ef17292ec4b8276f76
4rkhamknight01 <devanshdasindia@outlook.com>
2023-10-30 15:51:12 +0530

    final changes

--- nifty_intraday.py
+++ nifty_intraday.py
@@ -1,19 +1,26 @@
 import yfinance as yf
 import pandas as pd
+import datetime
 
-start_date = '2023-10-16'
-end_date = '2023-10-20'
+tod = datetime.datetime.now()
+d = datetime.timedelta(days=5)
+start_date = tod-d
+end_date = tod
 
-nifty50_tickers = ['ADANIPORTS.NS', 'ASIANPAINT.NS', 'AXISBANK.NS', 'BAJAJ-AUTO.NS']
+nifty50_tickers = ['^NSEI']
 
 data = yf.download(nifty50_tickers, start=start_date, end=end_date, interval='15m')
 
 data = data[['Open', 'High', 'Low', 'Close', 'Volume']]
 data.index.name = 'timestamp'
+print(data.index)
 
 filtered_data = data.loc[(data.index.time == pd.to_datetime('09:15:00').time()) | 
 
                            (data.index.time == pd.to_datetime('09:30:00').time()) | (data.index.time == pd.to_datetime('15:00:00').time()) | (data.index.time == pd.to_datetime('15:15:00').time())]
+
+filtered_data = filtered_data[[ 'Open', 'Close']]
+filtered_data.columns = ['Opening', 'Closing']
 
 print(data.head)
 print(filtered_data)
